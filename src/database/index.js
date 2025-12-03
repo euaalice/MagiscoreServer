ab89fe0d2b773b4ad5461fb9051fce9f7b8532ab
Felipe Belchior <aliceconceicao77@gmail.com>
2025-12-03 10:08:38 -0300

    1.0

---
+++ src/database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+//mongoose.connect('mongodb+srv://articusarname:Iron-man1573594646@articcluster.vnr4w.mongodb.net/');
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
